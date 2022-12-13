49071b2660db1f560cb4e5c19b7cc97377c097f1
Diego Priess <diegopriess.dev@gmail.com>
2022-12-12 23:51:50 -0300

    Commitando tudo que consegui, firebase não deu certo

--- lib/pages/add_new_imc.dart
+++ lib/pages/add_new_imc.dart
@@ -1,73 +1,63 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_imc/pages/my_home_page.dart';
 
-class AddNewImc extends StatelessWidget {
+class AddNewImc extends StatefulWidget {
   const AddNewImc({super.key, required this.title});
 
   final String title;
 
   @override
-  Widget build(BuildContext context) {
-    const appTitle = 'Form Validation Demo';
-
-    return MaterialApp(
-      title: appTitle,
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text(appTitle),
-        ),
-        body: const MyCustomForm(),
-      ),
-    );
-  }
+  State<AddNewImc> createState() => _MyCustomFormState();
 }
 
-class MyCustomForm extends StatefulWidget {
-  const MyCustomForm({super.key});
-
-  @override
-  MyCustomFormState createState() {
-    return MyCustomFormState();
-  }
-}
-
-class MyCustomFormState extends State<MyCustomForm> {
+class _MyCustomFormState extends State<AddNewImc> {
   final _formKey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
-    return Form(
-      key: _formKey,
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          TextFormField(
-            validator: (value) {
-              if (value == null || value.isEmpty) {
-                return 'Please enter some text';
-              }
-              return null;
-            },
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 16.0),
-            child: ElevatedButton(
-              onPressed: () {
-                if (_formKey.currentState!.validate()) {
-                  ScaffoldMessenger.of(context).showSnackBar(
-                    const SnackBar(content: Text('IMC adicionado com sucesso')),
-                  );
-                }
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => MyHomePage(title: "Calculadora de IMC")),
-                );
-              },
-              child: const Text('Confirmar'),
+    return Scaffold(
+        appBar: AppBar(
+          title: Text(widget.title),
+        ),
+        body: Center(
+          child: Form(
+            key: _formKey,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const FloatInput(
+                  labelText: 'Nome',
+                  inputPlaceholder: 'Informe o nome',
+                ),
+                const FloatInput(
+                  labelText: 'Altura',
+                  inputPlaceholder: 'Informe a altura',
+                ),
+                const FloatInput(
+                  labelText: 'Peso',
+                  inputPlaceholder: 'Informe o peso',
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    if (_formKey.currentState!.validate()) {
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        const SnackBar(
+                            content: Text('IMC adicionado com sucesso')),
+                      );
+                    }
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) =>
+                              MyHomePage(title: "Calculadora de IMC")),
+                    );
+                  },
+                  child: const Text('Confirmar'),
+                ),
+              ],
             ),
           ),
-        ],
-      ),
+        )
     );
   }
 }
